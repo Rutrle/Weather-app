0963b7c21aa9fd10704c672c4f94846c202d0142
Jakub <rutrle@seznam.cz>
2020-12-23 03:10:21 +0100

    getting actual temperature

--- exploring_page.py
+++ exploring_page.py
@@ -3,10 +3,14 @@ import pprint
 from bs4 import BeautifulSoup
 
 api_request = requests.get(
-    'https://www.in-pocasi.cz/predpoved-pocasi/cz/praha/praha-324/?den=6#day6')
+    'https://www.in-pocasi.cz/predpoved-pocasi/cz/praha/praha-324')
 
 # pprint.pprint(api_request.content)
 soup = BeautifulSoup(api_request.content, 'html.parser')
 results = soup.find(id='day1')
 results = results.find(class_="mt-1 strong")
 print(results.text)
+
+actual_temp = soup.find(class_='alfa mb-1')
+
+print(actual_temp.text)
