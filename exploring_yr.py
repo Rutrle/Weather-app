bda1ae4c146120ae00fd9cbfedc24f882539655d
Jakub <rutrle@seznam.cz>
2021-01-15 03:09:12 +0100

    getting needed data from yr.no

--- exploring_yr.py
+++ exploring_yr.py
@@ -12,4 +12,17 @@ header = {
 
 api_request = requests.get(url, headers=header)
 api_request = api_request.json()
-print(api_request)
+# print(api_request)
+
+with open('yr.json', 'w') as writefile:
+    json.dump(api_request, writefile, indent=4)
+
+
+relevant_data = api_request['properties']['timeseries']
+
+with open('cleaned_data.json', 'w') as f:
+    json.dump(relevant_data, f, indent=4)
+
+for weather_log in relevant_data:
+    print(weather_log['time'])
+    print(weather_log['data']['instant']['details']['air_temperature'])
